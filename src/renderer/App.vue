901fbad7b19036dadec766dc3c33403236240829
chief <chief_fei@163.com>
2020-07-31 23:42:52 +0800

    初始化项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 've'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
