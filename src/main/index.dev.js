814c90afc92f9ac3681e8ea9691e738c07c2ae0e
chief <chief_fei@163.com>
2020-08-01 13:50:15 +0800

    添加vue-devtool

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -8,17 +8,22 @@
 /* eslint-disable */
 
 // Install `electron-debug` with `devtron`
+import { BrowserWindow } from "electron";
+import path from 'path';
+
 require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
+  // 安装vue-devtools
+  BrowserWindow.addDevToolsExtension(path.resolve(__dirname, '../../devTools/vue-devtools'));
+})
+  /*let installExtension = require('electron-devtools-installer')
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
     .then(() => {})
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
-    })
-})
+    })*/
 
 // Require `main` process to boot app
 require('./index')
